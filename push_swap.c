643780c70591e21e911ef9d9f0e6f9d48379882f
Mael Guilbert Costes <mguilber@2B2.42angouleme.fr>
2025-12-04 20:14:00 +0100

    fck

--- push_swap.c
+++ push_swap.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 
 
-size_t	ft_strlen(int *str)
+size_t	ft_strlen(char *str)
 {
 	int	i;
 
@@ -15,16 +15,16 @@ size_t	ft_strlen(int *str)
 }
 
 
-int *push_swap(int *a) {
+int *push_swap(char *a) {
   int i = 1;
   int mid;
   mid = ft_strlen(a);
   printf("%i/ne", mid);
-  return a;
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
-//  printf("%i\n", *argv[2]);
-  int *a = push_swap((int *)argv[1]);
+  //printf("%i\n", *argv[2]);
+  int *a = push_swap(argv[1]);
   return 0;
 }
